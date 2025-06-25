3f292c6fe29c0456ec80f812be7aa125ba2d171f
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-25 11:20:28 -0300

    fix : Replace Basic log to Log4j2

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,19 @@ repositories {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
     implementation("org.springframework.boot:spring-boot-starter-security")
+
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.3.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
